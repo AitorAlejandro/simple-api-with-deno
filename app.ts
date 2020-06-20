e4b1c8224c382cc72b0d1315740a27f9d8d252c9
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2020-06-20 22:27:38 +0200

    book controller functions

--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
 import { Application } from "https://deno.land/x/abc@v1.0.0-rc8/mod.ts";
+import {
+  get_all_books,
+  get_book,
+  create_book,
+  delete_book,
+} from "./controllers/bookController.ts";
 
 const PORT = 3000;
 
 const app = new Application();
 
 app
-  .get("/books", () => {})
-  .get("/books/:id", () => {})
-  .post("/books", () => {})
-  .delete("/books/:id", () => {});
+  .get("/books", get_all_books)
+  .get("/books/:id", get_book)
+  .post("/books", create_book)
+  .delete("/books/:id", delete_book);
 
 // listen to port
 app.start({ port: PORT });
